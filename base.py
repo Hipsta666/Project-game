4a52a1266823387e34e7ff019d9d36355429a521
Hipsta666 <Andew-hipsta@mail.ru>
2018-10-28 17:06:19 +0700

    changed values

--- base.py
+++ base.py
@@ -10,8 +10,7 @@ while num_1 == num_2:
     num_2 = randint(0, 4)
 
 # drop two random letters
-f = b.replace(b[num_1], "")
-q = f.replace(f[num_2], "")
+word_m_sign = b.replace(b[num_1], "")
+new_word = f.replace(f[num_2], "")
 
 print(q, num_1, num_2)
-
